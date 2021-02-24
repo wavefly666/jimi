1eb565beeae278c6625b506fa18e2d4919cc99ff
wavefly666 <153584289@qq.com>
2021-02-24 19:39:30 +0800

    wanglang 01

---
+++ src/main/java/exer/Study.java
@@ -0,0 +1,9 @@
+package exer;
+
+public class Study {
+    public static void main(String[] args) {
+        System.out.println("good good study, day day up!");
+        System.out.println("good good study, day day up!");
+        System.out.println("good good study, day day up!");
+    }
+}
