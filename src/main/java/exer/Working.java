c1c50262b69659669045e0787ebe6c43938150c1
jimingjia666 <1048852817@qq.com>
2021-02-24 21:02:26 +0800

    working 01

---
+++ src/main/java/exer/Working.java
@@ -0,0 +1,16 @@
+package exer;
+
+public class Working {
+    public static void main(String[] args) {
+        System.out.println("working hard!");
+        System.out.println("working hard!");
+        System.out.println("working hard!");
+        System.out.println("working hard!");
+        System.out.println("working hard!");
+        System.out.println("working hard!");
+        System.out.println("working hard!");
+        System.out.println("working hard!");
+        System.out.println("working hard!");
+        System.out.println("working hard!");
+    }
+}
